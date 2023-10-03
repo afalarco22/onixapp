26351469f95e5690667e63ea29e04cd2bab4d29e
afalarco22 <afalarco@unl.edu.co>
2023-10-03 08:40:07 -0500

    agregando onixapp

---
+++ data/empelados.py
@@ -0,0 +1,15 @@
+empleado1 = {
+    "id":1,
+    "nombre":"Andrew",
+    "apellidos":"Alarcón",
+    "salariomensual": 150000000,
+    "deudas": True,
+    "retencionfuente":0.1,
+    "correo": "philipuslll@yahoo.es",
+    "telefono": "3008761828",
+    "cargo": "Arquitecto"
+}
+
+
+# DESSARROLLADOR JUNIO, INTERMEDIO AVANZADO, ARQUITECTO
+# solo se ganan más de 6 millones se aplica retefuente, si no poner cero
